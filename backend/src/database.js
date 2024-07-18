b7f3f5fa3a7633fd8a5fc3448c1f6c54fa44775d
Nubia Araujo <nubisana8@gmail.com>
2024-07-18 16:15:47 -0500

    Practica

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost:27017/usuarios_db';
+const URI = 'mongodb+srv://mongoatlas:admin@cluster0.4ih86bj.mongodb.net/datos?retryWrites=true&w=majority';
 mongoose.connect(URI)
 
-.then(db => console.log('Base de datos conectada'))
-.catch(err => console.log(err));
+//.then(db => console.log('Base de datos conectada'))
+//.catch(err => console.log(err));
 
-module.exports = mongoose;
+  .then(() => {
+    console.log('Conexión a MongoDB Atlas exitosa');
+  })
+  .catch((error) => {
+    console.error('Error conectándose a MongoDB Atlas:', error);
+  });
+
+//module.exports = mongoose;
+
